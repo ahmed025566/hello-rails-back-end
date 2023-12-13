e3ea4afd4945b3794627495181521af91c4d926f
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-13 11:17:36 +0200

    Add message table with greeting controller and api endpoint

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,6 @@
+class GreetingController < ApplicationController
+  def random
+    @random_message = Message.order(Arel.sql('RANDOM()')).first
+    render json: { greeting: @random_message.greeting }
+  end
+end
